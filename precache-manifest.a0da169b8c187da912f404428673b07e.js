312ea02408074b9a1179741308bda1677cb4387a
Simon Bates <simon@bitstructures.com>
2019-11-19 08:29:13 -0500

    Update gh-pages

---
+++ precache-manifest.a0da169b8c187da912f404428673b07e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f192df9056912c91b8d6f3bb966914b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b8679f14ff48d29c040",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "611fb5add1bc96336913",
+    "url": "/static/css/main.de867b6f.chunk.css"
+  },
+  {
+    "revision": "7b8679f14ff48d29c040",
+    "url": "/static/js/2.2d5ec1a7.chunk.js"
+  },
+  {
+    "revision": "d74114c97a77ce443394",
+    "url": "/static/js/3.58484722.chunk.js"
+  },
+  {
+    "revision": "611fb5add1bc96336913",
+    "url": "/static/js/main.30376440.chunk.js"
+  },
+  {
+    "revision": "cc0e9691eb7dac734206",
+    "url": "/static/js/runtime-main.3aa00fe2.js"
+  },
+  {
+    "revision": "22545845e23c778f557dd876e86bc7c8",
+    "url": "/static/media/ic_add_24px.22545845.svg"
+  },
+  {
+    "revision": "c9c3769f2f9b80e9441a255c2380827d",
+    "url": "/static/media/ic_arrow_back_48px.c9c3769f.svg"
+  },
+  {
+    "revision": "d27debbd7c553a0a0766772259ce58d8",
+    "url": "/static/media/ic_arrow_forward_48px.d27debbd.svg"
+  },
+  {
+    "revision": "a4558d3d024136728782a60e79abddf7",
+    "url": "/static/media/ic_arrow_upward_48px.a4558d3d.svg"
+  },
+  {
+    "revision": "e3722ad6761564eeb4d8fb121a50e4a0",
+    "url": "/static/media/ic_check_box_outline_blank_48px.e3722ad6.svg"
+  },
+  {
+    "revision": "addfce46ee90a4cc5f8d1aa464ab9822",
+    "url": "/static/media/ic_clear_24px.addfce46.svg"
+  },
+  {
+    "revision": "2f0a1732b3c18f0c37530b3690d7d092",
+    "url": "/static/media/ic_play_arrow_48px.2f0a1732.svg"
+  }
+]);
